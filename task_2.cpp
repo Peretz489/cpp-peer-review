81c23ad3dbfe29b05ebfce30e8c4bd2c5ae5f0c4
Peretz489 <peretz489@gmail.com>
2023-11-10 23:20:06 +0300

    review 1

--- task_2.cpp
+++ task_2.cpp
@@ -5,6 +5,10 @@
 #include <string_view>
 #include <vector>
 #include <execution>
+#include <cassert>
+
+// uncomment to run tests
+//#define DEBUG
 
 using namespace std;
 
@@ -18,7 +22,9 @@ public:
 
     bool operator==(const Domain &rhs)
     {
-        return this->IsSubdomain(rhs) || rhs.IsSubdomain(*this);
+        return this->GetName().size() <= rhs.GetName().size()
+                   ? rhs.IsSubdomain(*this)
+                   : this->IsSubdomain(rhs);
     }
 
     bool IsSubdomain(const Domain &other) const
@@ -27,12 +33,15 @@ public:
         {
             return false;
         }
-        auto last_symbol_ptr = reversed_domain_.begin();
-        std::advance(last_symbol_ptr, other.reversed_domain_.size());
-        return std::equal(std::execution::par, reversed_domain_.begin(), last_symbol_ptr, other.reversed_domain_.begin(), other.reversed_domain_.end());
+        else
+        {
+            auto last_symbol_ptr = reversed_domain_.begin();
+            std::advance(last_symbol_ptr, other.reversed_domain_.size());
+            return std::equal(std::execution::par, reversed_domain_.begin(), last_symbol_ptr, other.reversed_domain_.begin(), other.reversed_domain_.end());
+        }
     }
 
-    const std::string &GetName() const
+    const std::string GetName() const
     {
         return reversed_domain_;
     }
@@ -57,11 +66,15 @@ public:
 
     bool IsForbidden(const Domain &domain) const
     {
+        if (domain.GetName().size() == 1)
+        {
+            return true;
+        }
         auto position = std::upper_bound(forbidden_list_.begin(), forbidden_list_.end(), domain,
                                          [](const Domain &lhs, const Domain &rhs)
                                          {
-                                             std::string domain1 = lhs.GetName();
-                                             std::string domain2 = rhs.GetName();
+                                             const std::string &domain1 = lhs.GetName();
+                                             const std::string &domain2 = rhs.GetName();
                                              return std::lexicographical_compare(domain1.begin(), domain1.end(),
                                                                                  domain2.begin(), domain2.end());
                                          });
@@ -104,8 +117,82 @@ Number ReadNumberOnLine(istream &input)
     return num;
 }
 
+std::stringstream &TestForbidden(std::stringstream &out)
+{
+    out << "ru"s
+        << "\n";
+    out << "test.ru"s
+        << "\n";
+    out << "net.qu"s
+        << "\n";
+    out << "pp.net.du"s
+        << "\n";
+
+    return out;
+}
+
+std::stringstream &Test1(std::stringstream &out)
+{
+    out << ""s
+        << "\n";
+    out << "ru"s
+        << "\n";
+    out << "test.ru"s
+        << "\n";
+    out << "pp.net.qu"s
+        << "\n";
+    out << "aa.pp.net.du"s
+        << "\n";
+    return out;
+}
+
+std::stringstream &Test2(std::stringstream &out)
+{
+    out << ".u"s
+        << "\n";
+    out << "net.du"s
+        << "\n";
+    out << "qu"s
+        << "\n";
+    out << "pp.not.qu"s
+        << "\n";
+    out << "aa.pi.net.du"s
+        << "\n";
+    return out;
+}
+
+void RunTests()
+{
+    //-----domain checker init------
+    std::stringstream test_stream;
+    const std::vector<Domain> forbidden_domains = ReadDomains(TestForbidden(test_stream), 6);
+    DomainChecker checker(forbidden_domains.begin(), forbidden_domains.end());
+    test_stream.str() = ""s;
+    test_stream.clear();
+
+    //--------bad domains test--------
+    for (const Domain &domain : ReadDomains(Test1(test_stream), 5))
+    {
+        assert(checker.IsForbidden(domain));
+    }
+    test_stream.str() = ""s;
+    test_stream.clear();
+    std::cout << "bad domains test passed" << std::endl;
+
+    //--------good domains test---------
+    for (const Domain &domain : ReadDomains(Test2(test_stream), 4))
+    {
+        assert(!checker.IsForbidden(domain));
+    }
+    std::cout << "good domains test passed" << std::endl;
+}
+
 int main()
 {
+#ifdef DEBUG
+    RunTests();
+#endif
+
     const std::vector<Domain> forbidden_domains = ReadDomains(cin, ReadNumberOnLine<size_t>(cin));
     DomainChecker checker(forbidden_domains.begin(), forbidden_domains.end());
 
